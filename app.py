2fa98387f8c924166d33e4ce1be386ee7764f7a6
irahbt <irah.bt@gmail.com>
2020-10-20 16:05:46 +0000

    Add join form functionality

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -29,8 +30,33 @@ def all_plants():
     return render_template("all_plants.html", all_plants=all_plants)
 
 
-@app.route("/join")
+@app.route("/join", methods=["GET", "POST"])
 def join():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already in user, please try another")
+            return redirect(url_for("join"))
+
+        confirm_password = request.form.get("confirm_password")
+        password = request.form.get("password")
+
+        if confirm_password == password:
+            join = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(
+                    request.form.get("password"))
+            }
+            mongo.db.users.insert_one(join)
+            # put the new user into 'session' cookie
+            session["user"] = request.form.get("username").lower()
+            flash("success")
+        else:
+            flash("passwords don't match")
+            return redirect(url_for("join"))
     return render_template("join.html")
 
 

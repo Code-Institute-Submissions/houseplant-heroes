0eefc73fef697b7fad439954fe9898d5f77eac26
irahbt <irah.bt@gmail.com>
2020-11-09 15:10:46 +0000

    Add function to validate select

--- static/js/script.js
+++ static/js/script.js
@@ -4,16 +4,25 @@ $(document).ready(function () {
   $(".tooltipped").tooltip();
   $(".modal").modal();
   $(".carousel").carousel({
-      numVisible: "7",	
+    numVisible: "7",
   });
   // init Masonry
   var $grid = $(".grid").masonry({
     itemSelector: ".grid-item",
     gutter: 15,
-    fitWidth: true
+    fitWidth: true,
   });
   // layout Masonry after each image loads
   $grid.imagesLoaded().progress(function () {
     $grid.masonry("layout");
+  });
+
+  // valide select box
+  $("select[required]").css({
+    display: "block",
+    height: 0,
+    padding: 0,
+    width: 0,
+    position: "absolute",
   });
 });
